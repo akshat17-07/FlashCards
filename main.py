2c087ede48adb702a9a561d246b1940da65543f2
akshat harlalka <akshatharlalka.ah@gmail.com>
2021-09-20 20:01:49 -0400

    Version 1.0.0 Completed

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 from csv_module import csv_reader, csv_writer
+import random
 
+selected_course = ""
 
 class MainWindow(Screen):
     pass
@@ -16,11 +18,20 @@ class SelectWindow(Screen):
 
     def btn(self):
         if csv_reader(self.subject.text):
+            global selected_course
+            selected_course = self.subject.text.upper()
             return True
         return False
 
 class ReviewWindow(Screen):
-    pass
+    question = ObjectProperty(None)
+    answer = ObjectProperty(None)
+
+    def btn(self):
+        data = csv_reader(selected_course)
+        ran = random.choice(data[1:])
+        self.question.text = ran[0]
+        self.answer.text = ran[1]
 
 class AddWindow(Screen):
     question = ObjectProperty(None)
